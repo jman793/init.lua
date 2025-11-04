f331c8faab5e8c66cafed62ed07214d8d5b8290f
jman793 <jmarz793@yahoo.com>
2025-11-03 22:54:16 -0800

    Add super maven and format

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -27,9 +27,10 @@ vim.keymap.set("n", "<leader>em", "<cmd>e ~/Repos/dotfiles/nvim/lua/mappings.lua
 -- life changing
 vim.keymap.set("n", "<CR>", '@="m`o<C-V><Esc>``"<CR>')
 vim.keymap.set("n", "<S-CR>", '@="m`O<C-V><Esc>``"<CR>')
+vim.keymap.set("n", "ss", "i<Space><ESC>")
 
 vim.keymap.set("n", "<leader>th", function()
-  require("nvchad.themes").open()
+	require("nvchad.themes").open()
 end, { desc = "telescope nvchad themes" })
 
 -- Which Key
@@ -47,7 +48,7 @@ vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help)
 
 -- Diagnostic Mappings
 local go_to_error = function()
-  vim.diagnostic.goto_next { severity = vim.diagnostic.severity.ERROR }
+	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
 end
 
 vim.keymap.set("n", "<leader>dq", vim.diagnostic.setqflist, { desc = "LSP diagnostic qflist" })
@@ -61,55 +62,55 @@ vim.keymap.set("n", "-", require("oil").open, { desc = "Open parent directory." 
 vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>")
 
 -- Telescope Mappings
-local builtin = require "telescope.builtin"
+local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files)
 vim.keymap.set(
-  "n",
-  "<leader>fa",
-  "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
-  { desc = "Telescope find all files" }
+	"n",
+	"<leader>fa",
+	"<cmd>Telescope find_files follow=true no_ignore=true hidden=true<CR>",
+	{ desc = "Telescope find all files" }
 )
 vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope Live Grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope Search Buffers" })
 vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope Old Files" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope Help" })
 vim.keymap.set(
-  "n",
-  "<leader>fz",
-  "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-  { desc = "telescope find in current buffer" }
+	"n",
+	"<leader>fz",
+	"<cmd>Telescope current_buffer_fuzzy_find<CR>",
+	{ desc = "telescope find in current buffer" }
 )
 
 -- Trouble Mappings
 vim.keymap.set("n", "<leader>tq", "<cmd>Trouble quickfix toggle<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>tx", "<cmd>Trouble diagnostics toggle<cr>")
 vim.keymap.set(
-  "n",
-  "<leader>tX",
-  "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-  { desc = "Trouble Show Diagnostics In File" }
+	"n",
+	"<leader>tX",
+	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+	{ desc = "Trouble Show Diagnostics In File" }
 )
 
 -- UndoTree Mappings
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Harpoon Mappings
-local mark = require "harpoon.mark"
-local ui = require "harpoon.ui"
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-h>", function()
-  ui.nav_file(1)
+	ui.nav_file(1)
 end)
 vim.keymap.set("n", "<C-t>", function()
-  ui.nav_file(2)
+	ui.nav_file(2)
 end)
 vim.keymap.set("n", "<C-n>", function()
-  ui.nav_file(3)
+	ui.nav_file(3)
 end)
 vim.keymap.set("n", "<C-s>", function()
-  ui.nav_file(4)
+	ui.nav_file(4)
 end)
 
 -- Floating Terminal Mappings
@@ -117,23 +118,23 @@ vim.keymap.set("n", "<leader>tt", "<cmd>FloatingTerminal<cr>i", { desc = "Toggle
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-N>", { desc = "terminal escape terminal mode" })
 
 -- Gitsigns Mappings
-local gitsigns = require "gitsigns"
+local gitsigns = require("gitsigns")
 
 -- Navigation
 vim.keymap.set("n", "]c", function()
-  if vim.wo.diff then
-    vim.cmd.normal { "]c", bang = true }
-  else
-    gitsigns.nav_hunk "next"
-  end
+	if vim.wo.diff then
+		vim.cmd.normal({ "]c", bang = true })
+	else
+		gitsigns.nav_hunk("next")
+	end
 end, { desc = "Next Git Hunk" })
 
 vim.keymap.set("n", "[c", function()
-  if vim.wo.diff then
-    vim.cmd.normal { "[c", bang = true }
-  else
-    gitsigns.nav_hunk "prev"
-  end
+	if vim.wo.diff then
+		vim.cmd.normal({ "[c", bang = true })
+	else
+		gitsigns.nav_hunk("prev")
+	end
 end, { desc = "Prev Git Hunk" })
 
 -- Actions
@@ -141,11 +142,11 @@ vim.keymap.set("n", "<leader>gs", gitsigns.stage_hunk, { desc = "Git Stage Hunk"
 vim.keymap.set("n", "<leader>gr", gitsigns.reset_hunk, { desc = "Git Reset Hunk" })
 
 vim.keymap.set("v", "<leader>gs", function()
-  gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+	gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
 end, { desc = "Git Stage Highlighted" })
 
 vim.keymap.set("v", "<leader>gr", function()
-  gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+	gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
 end, { desc = "Git Reset Highlighted" })
 
 vim.keymap.set("n", "<leader>gus", gitsigns.undo_stage_hunk, { desc = "Git Undo Stage Hunk" })
@@ -156,13 +157,13 @@ vim.keymap.set("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Git Preview H
 vim.keymap.set("n", "<leader>gi", gitsigns.preview_hunk_inline, { desc = "Git Preview Hunk Inline" })
 
 vim.keymap.set("n", "<leader>gb", function()
-  gitsigns.blame_line { full = true }
+	gitsigns.blame_line({ full = true })
 end, { desc = "Git Blame" })
 
 vim.keymap.set("n", "<leader>gd", gitsigns.diffthis, { desc = "Git Diff" })
 
 vim.keymap.set("n", "<leader>gQ", function()
-  gitsigns.setqflist "all"
+	gitsigns.setqflist("all")
 end)
 vim.keymap.set("n", "<leader>gq", gitsigns.setqflist, { desc = "Git Hunks Qflist" })
 
@@ -177,43 +178,43 @@ vim.keymap.set("n", "<leader>td", gitsigns.toggle_deleted)
 -- vim.keymap.set("n", "<leader>dc", "<CMD>DapContinue<CR>", { desc = "Debugger Continue" })
 
 -- NeoTest Mappings
-local neo_test = require "neotest"
+local neo_test = require("neotest")
 vim.keymap.set("n", "<leader>tr", function()
-  neo_test.run.run {
-    suite = false,
-    testify = true,
-  }
+	neo_test.run.run({
+		suite = false,
+		testify = true,
+	})
 end, { desc = "Debug: Running Nearest Test" })
 
 vim.keymap.set("n", "<leader>tv", function()
-  neo_test.summary.toggle()
+	neo_test.summary.toggle()
 end, { desc = "Debug: Summary Toggle" })
 
 vim.keymap.set("n", "<leader>ts", function()
-  neo_test.run.run {
-    suite = true,
-    testify = true,
-  }
+	neo_test.run.run({
+		suite = true,
+		testify = true,
+	})
 end, { desc = "Debug: Running Test Suite" })
 
 vim.keymap.set("n", "<leader>td", function()
-  neo_test.run.run {
-    suite = false,
-    testify = true,
-    strategy = "dap",
-  }
+	neo_test.run.run({
+		suite = false,
+		testify = true,
+		strategy = "dap",
+	})
 end, { desc = "Debug: Debug Nearest Test" })
 
 vim.keymap.set("n", "<leader>to", function()
-  neo_test.output.open()
+	neo_test.output.open()
 end, { desc = "Debug: Open test output" })
 
 vim.keymap.set("n", "<leader>ta", function()
-  neo_test.run.run(vim.fn.getcwd())
+	neo_test.run.run(vim.fn.getcwd())
 end, { desc = "Debug: Open test output" })
 
 -- TODO Comments
-local todo_comments = require "todo-comments"
+local todo_comments = require("todo-comments")
 todo_comments.setup()
 
 vim.keymap.set("n", "<leader>ft", "<CMD>TodoTelescope<CR>", { desc = "Find Todo Comments" })
